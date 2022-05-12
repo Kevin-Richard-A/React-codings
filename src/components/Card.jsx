9b5fe71d284d0abde70e6b97ac96115dcf7fd8f0
Kevin-Richard-A <75072776+Kevin-Richard-A@users.noreply.github.com>
2022-05-12 09:43:18 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,29 +6,29 @@ const foods=[
         id:1,
         name:'Idli',
         price:35,
-        desc:'Idli is a savoury rice dish made from fermented lentils. It is a staple food of South Indian cuisine.',
+        desc:'Idli is made using stream',
         img:'https://www.indianhealthyrecipes.com/wp-content/uploads/2021/06/idli.jpg'
     },
     {
         id:2,
         name:'Dosa',
         price:45,
-        desc:'Dosa is a savoury rice dish made from fermented lentils. It is a staple food of South Indian cuisine.',
+        desc:'Dosa is a savoury rice dish made from fermented lentils.',
         img:'https://www.cookwithmanali.com/wp-content/uploads/2020/05/Masala-Dosa-500x500.jpg'
     },
     {
         id:3,
         name:'Puri',
         price:55,
-        desc:'Puri is a savoury rice dish made wheat. It is a staple food of South Indian cuisine.',
-        img:'https://cdn.cdnparenting.com/articles/2020/02/26144721/PURI-BHAJI-RECIPE.webp'
+        desc:'Chapathi is a savoury rice dish made wheat.',
+        img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.chitrasfoodbook.com%2F2015%2F01%2Fhow-to-make-soft-chapatichapathi-recipe.html&psig=AOvVaw3yb1MBLOwDuT2K-l4FpiFs&ust=1652415067822000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJDrofyL2fcCFQAAAAAdAAAAABAD'
     },
     {
         id:4,
         name:'Paratha',
         price:65,
-        desc:'Paratha is food of South Indian cuisine.',
-        img:'https://vegrecipebook.com/wp-content/uploads/2018/06/21-1200x675.jpg'
+        desc:'Parotta is food of South Indian cuisine.',
+        img:'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.outlookindia.com%2Foutlooktraveller%2Fexplore%2Fstory%2F70641%2Fdid-you-know-about-malabar-parotta&psig=AOvVaw2TRkNk36fHe6MGm_FEY6mE&ust=1652415129168000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCMDI-piM2fcCFQAAAAAdAAAAABAD'
     },
  
 ]
@@ -71,7 +71,7 @@ function Cards() {
         <button onClick={handleIncrement}>+</button>
         <button>Add to cart:{counter}</button>
         <button onClick={handleDecrement}>-</button>
-        <h2>@ the price of  ₹150 only /-</h2>
+        <h2>@ the price is just ₹150 only /-</h2>
 
         <p>Change color <p style={{color}}>{color}</p></p>
         
